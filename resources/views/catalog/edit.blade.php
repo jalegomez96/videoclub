7d49438e717aa299ee71c89673b688af199fe715
jalegomez96 <jalegomez96@gmail.com>
2022-02-22 10:23:28 -0500

    Implementacion de tabla movies

--- resources/views/catalog/edit.blade.php
+++ resources/views/catalog/edit.blade.php
@@ -16,27 +16,27 @@
 
                     <div class="form-group">
                         <label for="title">Título</label>
-                        <input type="text" name="title" id="title" class="form-control">
+                        <input type="text" name="title" id="title" value="{{$pelicula->title}}" class="form-control">
                     </div>
 
                     <div class="form-group">
                         <label for="year">Año</label>
-                        <input type="text" name="year" id="year" class="form-control">
+                        <input type="text" name="year" id="year" value="{{$pelicula->year}}" class="form-control">
                     </div>
 
                     <div class="form-group">
                         <label for="director">Director</label>
-                        <input type="text" name="director" id="director" class="form-control">
+                        <input type="text" name="director" id="director" value="{{$pelicula->director}}" class="form-control">
                     </div>
 
                     <div class="form-group">
                         <label for="poster">Poster</label>
-                        <input type="text" name="poster" id="poster" class="form-control">
+                        <input type="text" name="poster" id="poster" value="{{$pelicula->poster}}" class="form-control">
                     </div>
 
                     <div class="form-group">
                         <label for="synopsis">Resumen</label>
-                        <textarea name="synopsis" id="synopsis" class="form-control" rows="3"></textarea>
+                        <textarea name="synopsis" id="synopsis" class="form-control" rows="3">{{$pelicula->synopsis}}</textarea>
                     </div>
 
                     <div class="form-group text-center">
